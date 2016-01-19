d956f58368489c04160e8d6db4db57df803beee4
RWOverdijk <r.w.overdijk@gmail.com>
2016-01-19 03:37:03 +0100

    chore(test): Push browserNoActivityTimeout up

--- karma.conf.js
+++ karma.conf.js
@@ -33,6 +33,8 @@ module.exports = function (config) {
     // list of files to exclude
     exclude: [],
 
+    browserNoActivityTimeout: 30000,
+
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {},

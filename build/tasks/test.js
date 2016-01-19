e5acbd9833eccc90aa2267401b1c6f1464ec32b5
RWOverdijk <r.w.overdijk@gmail.com>
2016-01-19 02:58:50 +0100

    refactor(test): Updated karma usage

--- build/tasks/test.js
+++ build/tasks/test.js
@@ -1,46 +1,18 @@
-var gulp = require('gulp');
-var karma = require('karma').server;
+var gulp        = require('gulp');
+var KarmaServer = require('karma').Server;
 
 /**
  * Run test once and exit
  */
-gulp.task('test', function (done) {
-    karma.start({
-        configFile: __dirname + '/../../karma.conf.js',
-        singleRun: true
-    }, function(e) {
-        done();
-    });
-});
-
-/**
- * Watch for file changes and re-run tests on each change
- */
-gulp.task('tdd', function (done) {
-    karma.start({
-        configFile: __dirname + '/../../karma.conf.js'
-    }, function(e) {
-        done();
-    });
-});
-
-/**
- * Run test once with code coverage and exit
- */
-gulp.task('cover', function (done) {
-  karma.start({
+gulp.task('test', ['lint'], function(done) {
+  var karmaServer = new KarmaServer({
     configFile: __dirname + '/../../karma.conf.js',
-    singleRun: true,
-    reporters: ['coverage'],
-    preprocessors: {
-      'test/**/*.js': ['babel'],
-      'src/**/*.js': ['babel', 'coverage']
-    },
-    coverageReporter: {
-      type: 'html',
-      dir: 'build/reports/coverage'
-    }
-  }, function (e) {
+    singleRun : true
+  }, function() {
     done();
+
+    process.exit();
   });
+
+  karmaServer.start();
 });

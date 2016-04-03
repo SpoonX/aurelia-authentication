6984c590d5d7077ef300ad2d4ee1bb3fecc3e327
doktordirk <doktordirk@outlook.de>
2016-04-03 14:09:37 +0200

    feat(project): bundle into single file
    BREAKING CHANGE: all imports need to use 'aurelia-authenticaton'

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -6,7 +6,6 @@ var compilerOptions = require('../babel-options');
 var assign = Object.assign || require('object.assign');
 var through2 = require('through2');
 var concat = require('gulp-concat');
-var insert = require('gulp-insert');
 var rename = require('gulp-rename');
 var tools = require('aurelia-tools');
 var del = require('del');
@@ -15,45 +14,19 @@ var vinylPaths = require('vinyl-paths');
 // merged output file name. a folder of paths.packageName is temporarly created in build-dts
 var jsName = paths.packageName + '.js';
 
-
+// concats submodules into one file, compiles d.ts file and copies them to the dist folders
 gulp.task('build-dts', function() {
   var importsToAdd = []; // stores extracted imports
 
   return gulp.src(paths.tsSource)
     .pipe(tools.sortFiles())
     .pipe(through2.obj(function(file, enc, callback) {  // extract all imports to importsToAdd
-      if (file) {
-        file.contents = new Buffer(tools.extractImports(file.contents.toString('utf8'), importsToAdd));
-        this.push(file);
-      }
+      file.contents = new Buffer(tools.extractImports(file.contents.toString('utf8'), importsToAdd));
+      this.push(file);
       return callback();
     }))
-    .pipe(concat(jsName)) // concat all selected files to jsName (now without their imports)
-    .pipe(to5(assign({}, compilerOptions.dts()))); // compile to d.ts from file jsName. d.ts file is in folder paths.packageName
-});
-
-gulp.task('build-es2015',  ['build-html-es2015'], function() {
-  return gulp.src(paths.source)
-    .pipe(to5(assign({}, compilerOptions.es2015())))
-    .pipe(gulp.dest(paths.output + 'es2015'));
-});
-
-gulp.task('build-commonjs', ['build-html-commonjs'], function() {
-  return gulp.src(paths.source)
-    .pipe(to5(assign({}, compilerOptions.commonjs())))
-    .pipe(gulp.dest(paths.output + 'commonjs'));
-});
-
-gulp.task('build-amd', ['build-html-amd'], function() {
-  return gulp.src(paths.source)
-    .pipe(to5(assign({}, compilerOptions.amd())))
-    .pipe(gulp.dest(paths.output + 'amd'));
-});
-
-gulp.task('build-system', ['build-html-system'], function() {
-  return gulp.src(paths.source)
-    .pipe(to5(assign({}, compilerOptions.system())))
-    .pipe(gulp.dest(paths.output + 'system'));
+    .pipe(concat(jsName))
+    .pipe(to5(assign({}, compilerOptions.dts())));
 });
 
 gulp.task('copy-dts', function() {
@@ -71,6 +44,46 @@ gulp.task('remove-dts-folder', function() {
   return gulp.src([tdsFolder])
     .pipe(vinylPaths(del));
 });
+
+// concats modules into one file
+gulp.task('concat-modules', function() {
+  var importsToAdd = []; // stores extracted imports
+
+  return gulp.src(paths.source)
+    .pipe(tools.sortFiles())
+    .pipe(through2.obj(function(file, enc, callback) {  // extract all imports to importsToAdd
+      file.contents = new Buffer(tools.extractImports(file.contents.toString('utf8'), importsToAdd));
+      this.push(file);
+      return callback();
+    }))
+    .pipe(concat(jsName))
+    .pipe(gulp.dest(paths.output));
+});
+
+gulp.task('build-es2015',  ['build-html-es2015'], function() {
+  return gulp.src(paths.output + jsName)
+    .pipe(to5(assign({}, compilerOptions.es2015())))
+    .pipe(gulp.dest(paths.output + 'es2015'));
+});
+
+gulp.task('build-commonjs', ['build-html-commonjs'], function() {
+  return gulp.src(paths.output + jsName)
+    .pipe(to5(assign({}, compilerOptions.commonjs())))
+    .pipe(gulp.dest(paths.output + 'commonjs'));
+});
+
+gulp.task('build-amd', ['build-html-amd'], function() {
+  return gulp.src(paths.output + jsName)
+    .pipe(to5(assign({}, compilerOptions.amd())))
+    .pipe(gulp.dest(paths.output + 'amd'));
+});
+
+gulp.task('build-system', ['build-html-system'], function() {
+  return gulp.src(paths.output + jsName)
+    .pipe(to5(assign({}, compilerOptions.system())))
+    .pipe(gulp.dest(paths.output + 'system'));
+});
+
 
 gulp.task('build-html-es2015', function() {
   return gulp.src(paths.html)
@@ -95,10 +108,11 @@ gulp.task('build-html-system', function() {
 gulp.task('build', function(callback) {
   return runSequence(
     'clean',
-    ['build-es2015', 'build-commonjs', 'build-amd', 'build-system'],
     'build-dts',
     'copy-dts',
     'remove-dts-folder',
+    'concat-modules',
+    ['build-es2015', 'build-commonjs', 'build-amd', 'build-system'],
     callback
   );
 });

a56b38c8229db1d2d98a543b268b2ef97c7f7dc3
doktordirk <doktordirk@outlook.de>
2016-05-27 09:41:24 +0200

    test(authentication): add throw not found nested token

--- test/authentication.spec.js
+++ test/authentication.spec.js
@@ -315,6 +315,15 @@ describe('Authentication', () => {
       expect(fail).toThrow();
     });
 
+    it('Should throw if token not found in nested', () => {
+      const fail = () => authentication.getTokenFromResponse(
+        {tokenProp: {wrongTokenName: 'some'}},
+        'tokenProp',
+        'tokenName'
+      );
+      expect(fail).toThrow();
+    });
+
     it('Should return token if response has a string in tokenName in tokenRoot of tokenProp', () => {
       expect(authentication.getTokenFromResponse(
         {tokenProp: {tokenRoot1: {tokenRoot2: {tokenName: 'some'}}}},

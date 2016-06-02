ec97b2b44e1d89381aadb5f7521ed128fb5f25b0
RWOverdijk <r.w.overdijk@gmail.com>
2016-06-02 19:25:32 +0200

    chore(project): Fixed dependencies

--- config.js
+++ config.js
@@ -1,12 +1,6 @@
 System.config({
   defaultJSExtensions: true,
-  transpiler: "babel",
-  babelOptions: {
-    "optional": [
-      "runtime",
-      "optimisation.modules.system"
-    ]
-  },
+  transpiler: false,
   paths: {
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*"
